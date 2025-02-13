b56bb461785ea44c000faff33e1b34f01d02c627
Tivins <tivins@elabo.fr>
2025-02-13 23:41:28 +0100

    removing dependencies

--- examples/bpProgram/CanvasBPProgramExample.ts
+++ examples/bpProgram/CanvasBPProgramExample.ts
@@ -24,6 +24,68 @@ class CanvasBPProgramExample extends CanvasBP {
         elements.push(new BPContextItem("FlipFlopNode", () => new FlipFlopNode()));
         return elements;
     }
+
+    /**
+     * Override the validation when a link is created.
+     * @param emitToasts
+     */
+    validateLinking(emitToasts = false) {
+        if (!super.validateLinking(emitToasts)) {
+            return false;
+        }
+        if (!this.createLinkAnchor || !this.overAnchor) return false;
+
+        let anchorStart = this.createLinkAnchor.object
+        let anchorEnd = this.overAnchor.object
+
+        if (this.createLinkAnchor.node === this.overAnchor.node) {
+            if (emitToasts) {
+                Toasts.instance.addToastDefault('Not possible to link anchor of the same node. This will be changed in the future', 5, 'error')
+            }
+            return false;
+        }
+
+        if ((anchorStart.type === 'branch' && anchorEnd.type !== 'branch') || (anchorStart.type !== 'branch' && anchorEnd.type === 'branch')) {
+            if (emitToasts) Toasts.instance.addToastDefault('Branch should be linked together', 5, 'error')
+            return false;
+        }
+        if (anchorStart.type === 'branch' && anchorEnd.type === 'branch') {
+            const linkStartID = this.blueprint.getLinksOf(this.createLinkAnchor);
+            const linkEndID = this.blueprint.getLinksOf(this.overAnchor);
+            // if ((linkStartID > -1 || linkEndID > -1)) {
+            //     if (emitToasts) {
+            //         const msg = document.createElement('div');
+            //         msg.className = 'p-1';
+            //         msg.textContent = 'Branch cannot be lined twice';
+            //         const a = document.createElement('a');
+            //         a.href = '#';
+            //         a.className = 'button empty squared';
+            //         a.textContent = 'Unlink';
+            //
+            //         const tmpOverAnchor = this.overAnchor;
+            //         const tmpCreateAnchor = this.createLinkAnchor;
+            //         a.addEventListener('click', (e: MouseEvent) => {
+            //             e.preventDefault();
+            //             this.blueprint.unlink(tmpOverAnchor);
+            //             this.blueprint.link(tmpOverAnchor, tmpCreateAnchor);
+            //         });
+            //         Toasts.instance.addToast([msg, a], 5, 'error');
+            //     }
+            //     return false;
+            // }
+            if (this.createLinkAnchor.side === this.overAnchor.side) {
+                if (emitToasts) Toasts.instance.addToastDefault(`A branch 'out' cannot be linked to a branch 'in', and vice-versa.`, 5, 'error')
+                return false;
+            }
+        }
+
+        if (anchorStart.type !== 'any' && anchorEnd.type !== 'any' && anchorStart.type !== anchorEnd.type) {
+            if (emitToasts) Toasts.instance.addToastDefault('Types mismatch', 5, 'error')
+            return false;
+        }
+
+        return true;
+    }
 }
 customElements.define('canvas-bp-program-example', CanvasBPProgramExample);
 
--- src/CanvasBP.ts
+++ src/CanvasBP.ts
@@ -1,6 +1,6 @@
 import {CanvasMap} from "./CanvasMap";
 import {Blueprint} from "./Blueprint";
-import {BPNode} from "./BPNode";
+import {Anchor, BPNode} from "./BPNode";
 import {Toasts} from "./components/Toasts";
 import {Geom} from "./math/Geom";
 import {BPColors} from "./BPColors";
@@ -122,58 +122,7 @@ export class CanvasBP extends CanvasMap {
     }
 
     validateLinking(emitToasts = false) {
-        if (!this.createLinkAnchor) return false;
-        if (!this.overAnchor) return false;
-
-        let anchorStart = this.createLinkAnchor.object
-        let anchorEnd = this.overAnchor.object
-
-        if (this.createLinkAnchor.node === this.overAnchor.node) {
-            if (emitToasts) {
-                Toasts.instance.addToastDefault('Not possible to link anchor of the same node. This will be changed in the future', 5, 'error')
-            }
-            return false;
-        }
-
-        if ((anchorStart.type === 'branch' && anchorEnd.type !== 'branch') || (anchorStart.type !== 'branch' && anchorEnd.type === 'branch')) {
-            if (emitToasts) Toasts.instance.addToastDefault('Branch should be linked together', 5, 'error')
-            return false;
-        }
-        if (anchorStart.type === 'branch' && anchorEnd.type === 'branch') {
-            const linkStartID = this.blueprint.getLinksOf(this.createLinkAnchor);
-            const linkEndID = this.blueprint.getLinksOf(this.overAnchor);
-            if (linkStartID > -1 || linkEndID > -1) {
-                if (emitToasts) {
-                    const msg = document.createElement('div');
-                    msg.className = 'p-1';
-                    msg.textContent = 'Branch cannot be lined twice';
-                    const a = document.createElement('a');
-                    a.href = '#';
-                    a.className = 'button empty squared';
-                    a.textContent = 'Unlink';
-
-                    const tmpOverAnchor = this.overAnchor;
-                    const tmpCreateAnchor = this.createLinkAnchor;
-                    a.addEventListener('click', (e: MouseEvent) => {
-                        e.preventDefault();
-                        this.blueprint.unlink(tmpOverAnchor);
-                        this.blueprint.link(tmpOverAnchor, tmpCreateAnchor);
-                    });
-                    Toasts.instance.addToast([msg, a], 5, 'error');
-                }
-                return false;
-            }
-            if (this.createLinkAnchor.side === this.overAnchor.side) {
-                if (emitToasts) Toasts.instance.addToastDefault(`A branch 'out' cannot be linked to a branch 'in', and vice-versa.`, 5, 'error')
-                return false;
-            }
-        }
-
-        if (anchorStart.type !== 'any' && anchorEnd.type !== 'any' && anchorStart.type !== anchorEnd.type) {
-            if (emitToasts) Toasts.instance.addToastDefault('Types mismatch', 5, 'error')
-            return false;
-        }
-
+        if (!this.createLinkAnchor || !this.overAnchor) return false;
         return true;
     }
 
@@ -300,72 +249,76 @@ export class CanvasBP extends CanvasMap {
         }
     }
 
+    showFormField(node:BPNode,anchor:Anchor,side:string,name:string) {
+
+        if (!anchor.editable) {
+            return;
+        }
+        if (anchor.type === BPTypes.branch) {
+            return;
+        }
+
+        const index_link = this.blueprint.getLinksOf(new BPAnchorID(node, side, name));
+        const link = index_link > -1 ? this.blueprint.links[index_link] : null;
+        const remote_anchor = link ? (link.a.node === node ? link.b : link.a) : null;
+        const type_is_nullable = anchor.type.substring(0, 1) === '?';
+        const final_value = remote_anchor ? remote_anchor.object.value : (anchor.value ?? '');
+        const is_linked = side === 'left' && index_link > -1;
+
+        const wrap = DOMUtil.element('div', {
+            className: `flex p-05 flex-align ${is_linked || !anchor.editable ? `disabled muted` : ''}`
+        });
+        const label = DOMUtil.element('div', {className: 'pr-1 text-sm', textContent: anchor.label});
+        label.style.width = "200px";
+
+        // -- nullCheck
+        const nullCheck = DOMUtil.element('input', {type: "checkbox"}) as HTMLInputElement;
+        nullCheck.checked = anchor.value === null;
+        nullCheck.style.display = type_is_nullable ? '' : 'none';
+        nullCheck.addEventListener('change', e => {
+            input.style.display = nullCheck.checked ? 'none' : '';
+        });
+
+        // -- Input
+        const input = DOMUtil.element('input', {}) as HTMLInputElement;
+        switch (anchor.type) {
+            case BPTypes.float:
+                input.pattern = "[0-9]*\.[0-9]*";
+                input.value = final_value;
+                break;
+            case BPTypes.int:
+                input.pattern = "[1-9][0-9]*"
+                input.value = final_value;
+                break;
+            case BPTypes.image:
+                input.type = 'file';
+                break;
+            default:
+                input.value = final_value;
+        }
+        if (!is_linked) input.required = true;
+        input.style.padding = '2px 5px';
+        if (anchor.value === null) input.style.display = 'none';
+        const onChange =  (e:Event|null) => {
+            if (anchor.type === BPTypes.int) {
+                anchor.value = parseInt(input.value)
+            } else {
+                anchor.value = input.value
+            }
+        }
+        input.addEventListener('change', (e:Event) => onChange(e));
+        onChange(null);
+
+        // -- add to DOM
+        wrap.append(label, nullCheck, input)
+        this.ctxContainer.append(wrap)
+    }
     showNodeForm(node:BPNode) {
         this.ctxContainer.innerHTML = '';
         for (let side in node.anchors) {
             for (let name in node.anchors[side]) {
                 const anchor = node.anchors[side][name];
-                if (anchor.type === BPTypes.branch) {
-                    continue;
-                }
-                if (!anchor.editable) {
-                    continue;
-                }
-
-                const index_link = this.blueprint.getLinksOf(new BPAnchorID(node, side, name));
-                const link = index_link > -1 ? this.blueprint.links[index_link] : null;
-                const remote_anchor = link ? (link.a.node === node ? link.b : link.a) : null;
-                const type_is_nullable = anchor.type.substring(0, 1) === '?';
-                const final_value = remote_anchor ? remote_anchor.object.value : (anchor.value ?? '');
-                const is_linked = side === 'left' && index_link > -1;
-
-                const wrap = DOMUtil.element('div', {
-                    className: `flex p-05 flex-align ${is_linked || !anchor.editable ? `disabled muted` : ''}`
-                });
-                const label = DOMUtil.element('div', {className: 'pr-1 text-sm', textContent: anchor.label});
-                label.style.width = "200px";
-
-                // -- nullCheck
-                const nullCheck = DOMUtil.element('input', {type: "checkbox"}) as HTMLInputElement;
-                nullCheck.checked = anchor.value === null;
-                nullCheck.style.display = type_is_nullable ? '' : 'none';
-                nullCheck.addEventListener('change', e => {
-                    input.style.display = nullCheck.checked ? 'none' : '';
-                });
-
-                // -- Input
-                const input = DOMUtil.element('input', {}) as HTMLInputElement;
-                switch (anchor.type) {
-                    case BPTypes.float:
-                        input.pattern = "[0-9]*\.[0-9]*";
-                        input.value = final_value;
-                        break;
-                    case BPTypes.int:
-                        input.pattern = "[1-9][0-9]*"
-                        input.value = final_value;
-                        break;
-                    case BPTypes.image:
-                        input.type = 'file';
-                        break;
-                    default:
-                        input.value = final_value;
-                }
-                if (!is_linked) input.required = true;
-                input.style.padding = '2px 5px';
-                if (anchor.value === null) input.style.display = 'none';
-                const onChange =  (e:Event|null) => {
-                    if (anchor.type === BPTypes.int) {
-                        anchor.value = parseInt(input.value)
-                    } else {
-                        anchor.value = input.value
-                    }
-                }
-                input.addEventListener('change', (e:Event) => onChange(e));
-                onChange(null);
-
-                // -- add to DOM
-                wrap.append(label, nullCheck, input)
-                this.ctxContainer.append(wrap)
+                this.showFormField(node,anchor,side,name);
             }
         }
     }
